8b0f374201bb80e15b93b25310469a782c852727
zsoe <zwemsoe@gmail.com>
2021-08-23 15:35:44 -0700

    testing error page

--- next-app/pages/_error.js
+++ next-app/pages/_error.js
@@ -1,16 +1,20 @@
-function Error({ statusCode }) {
+function Error({ statusCode, err }) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
+    <>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+      <p style={{ color: 'white' }}>{err.message}</p>
+      <p style={{ color: 'white' }}>{JSON.stringify(err)}</p>
+    </>
   );
 }
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+  return { statusCode, err };
 };
 
 export default Error;
