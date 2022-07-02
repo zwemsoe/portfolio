1b8cff37de46334e464c252ada9dde948e1bd73f
jay <56790194+zwemsoe@users.noreply.github.com>
2022-07-01 18:11:44 -0700

    update resume link

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -9,4 +9,4 @@ export const BLOG_DB = 'blogs-db';
 export const BLOGS_COLLECTION = 'blogs';
 export const BLOGS_DEV_COLLECTION = 'blogs-dev';
 export const RESUME_LINK =
-  'https://drive.google.com/file/d/1oVgJbPwL-4IssEJaF6xY2GbTsGWt_7Rp/view?usp=sharing';
+  'https://drive.google.com/file/d/13ZKgV5qArbbZcJPzLlXCQ3HXLMe9OgsG/view?usp=sharing';
